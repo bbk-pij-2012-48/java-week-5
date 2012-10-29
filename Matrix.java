5f14bb0ed75847207290c7c48e2ed45f5bd79cce
Richard Bennett <rbenne06@dcs-bbk-ac-uk>
2012-10-29 19:36:17 +0000

    matrix methods added but not yet tested

--- Matrix.java
+++ Matrix.java
@@ -28,4 +28,19 @@ public class Matrix{
 		}
 		System.out.println("");
 	}
+	
+	public void setRow(int row, String value){
+		if(row<matrix.length){ 				// valid row reference
+			int commaCount = 0;
+			for(int i=0 ; i<value.length() ; i++){
+				if(value.charAt(i)==',')
+					commaCount++;
+			}
+			if(commaCount==matrix[0].length-1){	// valid value string
+				String[] valueArray = value.split("\\,");
+			}
+			//continue here
+		}
+	}
+			
 }
