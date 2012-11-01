fcdace9c6c72b661533517919bc304ab90ebd63f
Richard Bennett <rbennett485-gmail-com>
2012-11-01 10:12:43 +0000

    finished

--- q7.groovy
+++ q7.groovy
@@ -1,6 +1,7 @@
 boolean finished = false;
 int employeeCount = 0;
 String tempName = "";
+String holdId = "";
 int tempId = 0;
 GrowingArray employees = new GrowingArray(1);
 println "To terminate, leave name blank or enter 0 for id";
@@ -25,9 +26,24 @@ while(!finished){
 	print "Enter employee name: ";
 	tempName = System.console().readLine();
 	print "Enter employee id: ";
-	tempId = Integer.parseInt(System.console().readLine());
+	holdId = System.console().readLine();
+	if(holdId.length()==0){
+		tempId = 0;
+	}
+	else{
+		tempId = Integer.parseInt(holdId);
+	}
 	
 	if(tempName.length()==0||tempId==0){
 		finished = true;
+	}
+}
+
+int[] id = employees.getId();
+String[] name = employees.getName();
+
+for(int i=0 ; i<employeeCount ; i++ ){			// Print staff with even id, or name starting S
+	if(id[i]%2==0||name[i].charAt(0)=='S'){
+		println name[i] + "  " + id[i];
 	}
 }
